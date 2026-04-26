78cd42e63c8263df35b02c633250c2e1e1bf9bff
squeeze-scanner <ggmelrose@googlemail.com>
2026-04-26 19:11:07 +0100

    Update universe_builder.py

--- universe_builder.py
+++ universe_builder.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+def load_sp500_universe():
+    try:
+        url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+        table = pd.read_html(url)[0]
+        tickers = table["Symbol"].tolist()
+        return [t.replace(".", "-") for t in tickers]
+    except:
+        return []
